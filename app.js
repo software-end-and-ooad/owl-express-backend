0440bb6f7b0b47977dfde99dd99cd6f52dd01bd9
Jitiwattana <bbos1@windowslive.com>
2017-08-14 20:15:52 +0700

    FIX cors error
    fronend angular cannot request this

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(require('./route/app-route'))
 
