a8a7835060abe4483e84f6af14f61c0826dad836
Jitiwattana <bbos1@windowslive.com>
2017-08-19 21:46:59 +0700

    INIT refresh token functoin

--- controller/refreshToken.controller.js
+++ controller/refreshToken.controller.js
@@ -1,8 +1,21 @@
 import Validator from 'validatorjs'
+import handler from './handlers/handlers'
+import jwt from 'jsonwebtoken'
+
+import jwtconfig from '../config/jwtconfig'
+
 
 function RefreshTokenController(req, res) {
 
-  res.status(200).json({ sucess: true })
+  const header = req.headers['authorization']
+  const token = handler.getTokenFormHeader(header)
+
+  jwt.verify(token, jwtconfig.secret, async function(err, decoded) {
+
+
+    res.status(200).json({ sucess: true })
+  })
+
 }
 
 module.exports = RefreshTokenController
