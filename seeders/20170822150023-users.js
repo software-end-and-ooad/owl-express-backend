e9b64b14acf81b0170bcbb3ec95843ee7993209e
Jitiwattana <bbos1@windowslive.com>
2017-08-22 22:06:50 +0700

    INIT file seeder

---
+++ seeders/20170822150023-users.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+     *queryInterface.createTable(
+     *  'users',
+     *  [{
+     *    name: 'test',
+     *    username: 'test',
+     *    email: 'test@kmitl.ac.th',
+     *    domain: 'test',
+     *    role: 'student',
+     *    studentid: 'test',
+     *    faculty: 'test',
+     *    confirm: 'test',
+     *    token_confirm: 'test',
+     *    password: 'test',
+     *    ftp_password: 'test',
+     *    remember_token: 'test',
+     *    created_at: new Date(),
+     *    updated_at: new Date(),
+     *  }],
+     *)
+     */
+  },
+
+  down: function (queryInterface, Sequelize) {
+  }
+};
