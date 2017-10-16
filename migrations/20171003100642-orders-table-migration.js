9fe1f7c3bcbf068c671adb204dcf94f6ce00877e
Jitiwattana <bbos1@windowslive.com>
2017-10-16 19:49:29 +0700

    FIX field size: defaultValue = '' for frontend value ''

--- migrations/20171003100642-orders-table-migration.js
+++ migrations/20171003100642-orders-table-migration.js
@@ -21,7 +21,8 @@ module.exports = {
         },
         size: {
           type: Sequelize.STRING,
-          allownull: true
+          allownull: true,
+          defaultValue: ''
         },
         price: {
           type: Sequelize.INTEGER,
